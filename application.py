e323dfd05dd4e4617c5655028da75cfcb32b5f16
asitshastri <asitshastri@gmail.com>
2023-08-22 16:55:29 +0530

    project

--- application.py
+++ application.py
@@ -1,23 +1,41 @@
-from flask import Flask, request,jsonify,render_template
 import pickle
+from flask import Flask,request,jsonify,render_template
 import numpy as np
-from sklearn.preprocessing import StandardScaler
 import pandas as pd
-
-##working on flask
+from sklearn.preprocessing import StandardScaler
 
 application = Flask(__name__)
-app = application
+app=application
 
-#import lasso regression and standard scaler pickle
-lasso_model = pickle.load(open("models/lasso_model.pk1"),'rb')
-scaler_model = pickle.load(open("models/scaler_lasso_model.pk1"),'rb')
+## import ridge regresor model and standard scaler pickle
+ridge_model=pickle.load(open('models/ridge.pkl','rb'))
+standard_scaler=pickle.load(open('models/scaler.pkl','rb'))
 
-#creating homepage
+## Route for home page
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='POST':
+        Temperature=float(request.form.get('Temperature'))
+        RH = float(request.form.get('RH'))
+        Ws = float(request.form.get('Ws'))
+        Rain = float(request.form.get('Rain'))
+        FFMC = float(request.form.get('FFMC'))
+        DMC = float(request.form.get('DMC'))
+        ISI = float(request.form.get('ISI'))
+        Classes = float(request.form.get('Classes'))
+        Region = float(request.form.get('Region'))
+
+        new_data_scaled=standard_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
+        result=ridge_model.predict(new_data_scaled)
+
+        return render_template('home.html',result=result[0])
+
+    else:
+        return render_template('home.html')
 
 
 if __name__=="__main__":
